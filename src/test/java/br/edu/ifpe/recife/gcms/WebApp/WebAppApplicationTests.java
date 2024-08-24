99714d5f4258b4c2e9ec7e70f7b87404c1210849
Gustavo Melo <108017300+Gust4voMelo@users.noreply.github.com>
2024-08-23 21:23:52 -0300

    Update WebAppApplicationTests.java
    Erro para a pratica 05 jenkins

--- src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
+++ src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
@@ -33,7 +33,7 @@ class WebAppApplicationTests {
 		this.mockMvc.perform(get("/Gustavo"))
 			.andDo(print()).andExpect(status().isOk())
 			.andExpect(content()
-					.string(containsString("Hello, Gustavo")));
+					.string(containsString("Hello, Gustavo, COM ERRO!")));
 	}
 	
 	@Test
